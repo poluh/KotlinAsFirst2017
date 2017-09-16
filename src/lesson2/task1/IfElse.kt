7249d7acc27e5334e3a61b98fd24a2292dc5b913
sergey <s.poluh@ro.ru>
2017-09-16 17:19:31 +0300

    A ease lesson that will be finished in the evening

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -1,4 +1,5 @@
 @file:Suppress("UNUSED_PARAMETER")
+
 package lesson2.task1
 
 import lesson1.task1.discriminant
@@ -33,8 +34,20 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * Мой возраст. Для заданного 0 < n < 200, рассматриваемого как возраст человека,
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
-fun ageDescription(age: Int): String = TODO()
+fun ageDescription(age: Int): String {
 
+
+    if (age % 100 in 5..20)
+        return "$age лет"
+    else if (age % 10 == 1)
+        return "$age год"
+    else if (age % 10 in 2..4)
+        return "$age года"
+    else
+        return "$age лет"
+
+
+}
 
 
 /**
@@ -59,7 +72,20 @@ fun timeForHalfWay(t1: Double, v1: Double,
  */
 fun whichRookThreatens(kingX: Int, kingY: Int,
                        rookX1: Int, rookY1: Int,
-                       rookX2: Int, rookY2: Int): Int = TODO()
+                       rookX2: Int, rookY2: Int): Int {
+
+    if ((kingX == rookX1 || kingX == rookX2) && (kingY == rookY1 || kingY == rookY2))
+        return 3
+    else
+        if ((kingX == rookX1 || kingY == rookY1) && (kingX != rookX2 && kingY != rookY2))
+            return 1
+        else
+            if ((kingX == rookX2 || kingY == rookY2) && (kingX != rookX1 && kingY != rookY1))
+                return 2
+            else
+                return 0
+
+}
 
 /**
  * Простая
--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -17,7 +17,13 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean {
+
+    if ((number%1000 + (number%100)/10) == (number/1000 + ((number/100)%10)))
+        return true
+    else
+        return false
+}
 
 /**
  * Простая
@@ -36,7 +42,16 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Вернуть true, если утверждение верно
  */
 fun circleInside(x1: Double, y1: Double, r1: Double,
-                 x2: Double, y2: Double, r2: Double): Boolean = TODO()
+                 x2: Double, y2: Double, r2: Double): Boolean{
+
+    if ((x1 == x2) || (y1 == y2) || (x1 + y1) == (x2 + y2) || (x1 - y1) == (x2 - y2))
+        return true
+    else
+        return false
+
+}
+
+
 
 /**
  * Средняя
@@ -47,4 +62,12 @@ fun circleInside(x1: Double, y1: Double, r1: Double,
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean{
+
+    if ((a == r)  && (b == s))
+        return true
+    else
+        return false
+
+
+}
